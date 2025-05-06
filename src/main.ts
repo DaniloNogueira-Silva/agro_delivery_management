09508ab0ebd0ad6d653db1cb70a1bb8d557f0380
DaniloNogueira-Silva <danilo.nogueira1802@gmail.com>
2025-05-06 16:34:12 -0300

    feat: :sparkles: Habilitando cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,15 @@ import { NestFactory } from '@nestjs/core';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors(
+    {
+      origin: 'http://localhost:3000',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    }
+  );
+
   // Configuração do Swagger
   const config = new DocumentBuilder()
     .setTitle('AgroVerde Logística API')
